92aa15a6f6cf1b013b83ed3657a3b1f5d02783b6
zanehill01 <zane_hill1@baylor.edu>
2023-02-13 13:50:39 -0600

    Done

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -8,29 +8,48 @@ You will create a dictionary that has the full employee name as the key and ONLY
 Iternate through the dictionary to print out their name and thier new salary (as in image)
 '''
 
+# ID,First Name,Last Name,Department,Title,Salary,Hire Date,Birth Date,Gender,Clearance
+
 import csv
 
 #open the file
 
+EMPLOYEE_DATA = 'employee_data.csv'
 
-
+infile = open(EMPLOYEE_DATA, 'r', newline='')
+reader = csv.reader(infile)
+next(reader)
 
 #create an empty dictionary
 
+employee_dict = {'Employees': []}
+temp_dict = {}
 
 #use a loop to iterate through the csv file
 
+for row in reader:
 
     #check if the employee fits the search criteria
 
+    Name = row[1] + row[2]
 
-    
+    temp_dict['Name'] = Name
+    temp_dict['NewSalary'] = int(int(row[5]) * .1 + int(row[5]))
+
+    employee_dict['Employees'].append(temp_dict)
+    temp_dict = {}
+
+    print('Manager Name:', row[1], row[2], 'Current Salary:', row[5])
+
+#iternate through the dictionary and print out the key and value as per printout
 
 print()
 print('=========================================')
 print()
 
-#iternate through the dictionary and print out the key and value as per printout
+for row in range(len(employee_dict['Employees'])):
+
+    print('Manager Name:', employee_dict['Employees'][row]['Name'], 'New Salary:', employee_dict['Employees'][row]['NewSalary'])
 
 
 
